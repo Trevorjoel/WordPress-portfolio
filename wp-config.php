50d0c4012668420da4600ae92b2f513e5d01f4b5
Trevorjoel <trevsstuff@hotmail.com>
2019-04-08 13:25:33 +1000

    create gitignore

--- wp-config.php
+++ wp-config.php
@@ -45,7 +45,7 @@ define( 'DB_COLLATE', '' );
  * Change these to different unique phrases!
  * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
  * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
+ * awd
  * @since 2.6.0
  */
 define( 'AUTH_KEY',         'put your unique phrase here' );
